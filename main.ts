cdc5c5a96f3934f79561ef9da7cfe0dbba82d568
ubai-d <ubaidurrehman14827@gmail.com>
2024-03-24 12:31:47 +0500

    initial commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+const percentage:number = 110;
+
+if(percentage >= 90 && percentage <=100){
+    console.log("A+");
+}
+else if(percentage >=80 && percentage <=90){
+    console.log("A");
+}
+else if(percentage >=70 && percentage <=80){
+    console.log("B");
+}
+else if(percentage >=60 && percentage <=70){
+    console.log("C");
+
+}
+else{
+    console.log("App Fail hein");
+    
+}
